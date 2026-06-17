db667dd0c22f669acbc95b1235ba1d33f97f2219
AwebSystemGM <awebsystemagj@gmail.com>
2026-06-17 17:21:58 -0300

    feat: adicionar botao ver detalhes nos projetos e refatorar css

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botoesDesc = document.querySelectorAll(".btn-toggle-desc");
 
-const menuBtn = document.querySelector(".menu-toggle")
-const menumobile = document.querySelector(".ancoras")
+  botoesDesc.forEach((botao) => {
+    botao.addEventListener("click", function () {
+      const container = this.closest(".container-padrao");
+      const projetoPrincipal = container.querySelector(".projeto-principal");
+      
+      let descMobile = container.querySelector(".desc-dinamica-mobile");
 
-menuBtn.addEventListener("click", () => {
-  menumobile.classList.toggle("active")
-})
+      if (descMobile) {
+        descMobile.remove();
+        this.textContent = "Ver detalhes";
+      } else {
+        const textoDescricao = this.getAttribute("data-desc");
+
+        descMobile = document.createElement("p");
+        descMobile.classList.add("desc-dinamica-mobile");
+        descMobile.textContent = textoDescricao;
+
+        // Insere dinamicamente abaixo da estrutura de imagem/botão
+        projetoPrincipal.insertAdjacentElement("afterend", descMobile);
+        this.textContent = "Fechar";
+      }
+    });
+  });
+});
